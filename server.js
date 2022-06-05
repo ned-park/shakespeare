153f33c051eade906d522d5420d6871449c7cc45
ned-park <87483870+ned-park@users.noreply.github.com>
2022-06-05 19:01:23 -0400

    Adds cors, updates npm run scripts

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import 'dotenv/config';
+import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 
 const PORT = process.env.PORT || 8000
 const app = express()
+app.use(cors())
 
 app.use(express.urlencoded({ extended: true }));
 
